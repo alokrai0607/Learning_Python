6ad9d73bfb575f7f9fe78af47a9cc5a50fba35da
alokrai0607 <saelalok001@gmail.com>
2024-08-30 17:11:25 +0530

    Smallest

---
+++ findSmallest.py
@@ -0,0 +1,14 @@
+numbers = [3, 1, 4, 1, 0,5, 9, 2, 6]
+
+smallest = numbers[0]
+for i in numbers:
+    if(smallest>i):
+        smallest=i
+print(smallest)
+
+
+#Smallest
+
+numbers = [3, 1, 4, 1, 0,5, 9, 2, 6]
+smallest = min(numbers)
+print(smallest)
