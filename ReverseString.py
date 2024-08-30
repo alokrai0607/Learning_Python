bcf3183535d354887657d87571b56079a38f4661
alokrai0607 <saelalok001@gmail.com>
2024-08-30 16:15:54 +0530

    StringReversal

--- ReverseString.py
+++ ReverseString.py
@@ -1,11 +1,12 @@
  #  Way 1
 name = "ALOK"
-reverseName = name[::-1]
+reverseName = name[::-1]#is a Pythonic way to reverse a string using slicing
 print(reverseName)
 
 #Way 2 
 name = "Alok Kumar Rai"
 bag = ""
+# (for i in range(len(name)length , Stop , Step):)
 for i in range(len(name)-1,-1,-1):
     bag += name[i]
 print(bag)
@@ -15,7 +16,7 @@ def reverse_string(string):
     if len(string) == 0:
         return string
     else:
-        return reverse_string(string[1:]) + string[0]
+        return reverse_string(string[1:]) + string[0]#( slices the string to get all characters except the first one)
 my_string = "Alok Kumar Rai"
 rev_string = reverse_string(my_string)
 print(rev_string)
