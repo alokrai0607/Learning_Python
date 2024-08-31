61e07f710f835d177188ea5635ac11dbc30ce7c0
alokrai0607 <saelalok001@gmail.com>
2024-08-31 11:20:12 +0530

    CountDigit

---
+++ CountDigits.py
@@ -0,0 +1,19 @@
+# def countfun(st):
+#     bag = ""
+#     count = 0
+#     for i in st:
+#         count += 1
+#         bag += str(count)
+#        # print(count)
+#     return bag
+
+# print(countfun("Alok"))
+
+
+def countfun(st):
+    count = 0
+    for i in st:
+        count += 1
+    return count
+
+print(countfun("Alok Kumar Rai"))
