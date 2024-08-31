bb9353c4cf995b8d8306cc9c14f2d31dd6689e56
alokrai0607 <saelalok001@gmail.com>
2024-08-31 15:17:43 +0530

    printInt

---
+++ PrintInt.py
@@ -0,0 +1,15 @@
+def printInt(n):
+    if (n>0) :
+        print(f"{n}"" : is Positive Integer")
+    elif (n==0) :
+        print(f"{n}"" : is Zero Integer")
+    elif (n<0) :
+        print(f"{n}"" : is Negative Integer")
+
+print(printInt(-1))
+print(printInt(0))
+print(printInt(1))
+print(printInt(-11))
+printInt(55)
+printInt(100)
+
