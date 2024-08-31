4cd1d4d1aa89250c0f6a5554684b728bb5e15cf2
alokrai0607 <saelalok001@gmail.com>
2024-08-31 13:22:08 +0530

    Palindrom function

--- Palindrome.py
+++ Palindrome.py
@@ -1,6 +1,6 @@
+#Way 1
 name = "radar"
 bag = ""
-
 for i in range(len(name)-1, -1, -1):
     bag += name[i]
 
@@ -8,3 +8,17 @@ if bag == name:
     print("It is a palindrome")
 else:
     print("It is not a palindrome")
+
+#Way 2 
+def check_palindrome(word):
+    container=""
+    for i in range(len(word)-1,-1,-1):
+        container += word[i]
+
+    if word==container:
+        print("It is Palindrome")
+    else:
+        print("It is not a Palindrome")
+
+print(check_palindrome("Hello"))
+print(check_palindrome("MadaM"))
