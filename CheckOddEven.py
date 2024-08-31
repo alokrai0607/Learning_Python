d4b5246ead0c1d374496856664e4f956c47beccc
alokrai0607 <saelalok001@gmail.com>
2024-08-31 14:12:35 +0530

    EvenOrOdd

---
+++ CheckOddEven.py
@@ -0,0 +1,13 @@
+def chkEvenOdd(n):
+    if(n%2==0):
+        print(f"{n}" "  is : Even")
+    elif(n%2==1): 
+        print(f"{n}" "  is : Odd")
+
+print(chkEvenOdd(1))
+print(chkEvenOdd(5))
+print(chkEvenOdd(10))
+print(chkEvenOdd(15))
+print(chkEvenOdd(11))
+print(chkEvenOdd(21))
+print(chkEvenOdd(100))
