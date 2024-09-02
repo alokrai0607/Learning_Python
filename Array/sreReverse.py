e43aef463dbccf40d9cf8eaaa69f95385206d95c
alokrai0607 <saelalok001@gmail.com>
2024-09-02 15:32:42 +0530

    strReversePal

---
+++ Array/sreReverse.py
@@ -0,0 +1,21 @@
+list = [1,2,1]
+
+list_copy = list.copy()
+list_copy.reverse()
+
+if(list == list_copy):
+    print("it is pellindrome")
+else:
+    print("Not Pellindrome")
+
+#Check Pellindrome on pellindrome check .
+
+str = ["a","l","o","k","k","o","l","a","m"]
+strcopy = str.copy()
+strcopy.reverse()
+
+if(strcopy == str):
+    print("Pellindrome")
+else:
+    print("Not Pellindrome")
+    
