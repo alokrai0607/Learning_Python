db483a9d3d6ce635d6007036d2c874f53124c621
alokrai0607 <saelalok001@gmail.com>
2024-09-02 19:07:31 +0530

    unioInterSection

---
+++ unionIntersection.py
@@ -0,0 +1,9 @@
+
+set1 = {1,2,3}
+set2 = {2,3,4,5}
+
+#union will return all value from both sets .
+print(set1.union(set2)) #{1, 2, 3, 4, 5}
+#intersection will return only comman values from all sets .
+print(set1.intersection(set2))  #{2, 3}
+
