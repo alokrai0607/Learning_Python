ad06dd8993f6b7c503cf812f98cdcfccc206a3cd
alokrai0607 <saelalok001@gmail.com>
2024-09-02 18:14:09 +0530

    dict.py

---
+++ dict.py
@@ -0,0 +1,18 @@
+EmpInfo={
+    "EmpName" : "Alok Kumar RAi",
+    "PefLang" : ["Java", "Python","C++","JScript"],
+    "coNo" : ('9044414580','7880667919','8176015031'),
+    "age":29,
+    "Male":'true'
+}
+
+print(EmpInfo) #{'EmpName': 'Alok Kumar RAi', 'PefLang': ['Java', 'Python', 'C++', 'JScript'], 'coNo': ('9044414580', '7880667919', '8176015031'), 'age': 29, 'Male': 'true'}
+print(type(EmpInfo))
+
+print(EmpInfo["EmpName"])
+print(EmpInfo["PefLang"])
+print(EmpInfo["coNo"])
+print(EmpInfo["age"])
+print(EmpInfo["Male"])
+
+print(EmpInfo)
