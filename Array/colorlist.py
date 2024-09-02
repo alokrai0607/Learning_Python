1dbf37bf4579c77cebafe203816943a257ec7764
alokrai0607 <saelalok001@gmail.com>
2024-09-02 11:02:28 +0530

    colorList

---
+++ Array/colorlist.py
@@ -0,0 +1,14 @@
+#color_list = ["Red","Green","White" ,"Black"]
+#Write a Python program to display the first and last colors from the following list.
+
+#Way 1
+color_list = ["Red","Green","White" ,"Black"]
+print(color_list[0]) #Red
+print((color_list[len(color_list)-1])) #Black
+
+#Way 2
+color_list = ["Red","Green","White" ,"Black"]
+print (color_list[0]) #Red
+print (color_list[-1]) #Black
+
+
