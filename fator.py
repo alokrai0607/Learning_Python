7655f36046a40a651d52a4fcc481c9665da77446
alokrai0607 <saelalok001@gmail.com>
2024-09-03 15:40:46 +0530

    factorial

--- fator.py
+++ fator.py
@@ -3,3 +3,14 @@ prod = 1
 for i in range(1,inp+1):
     prod *= i
 print(prod)
+
+
+#i am writing factorial function .
+
+def fun_fact(n):
+    product=1
+    for i in range(1,n+1):
+        product *= i
+    return product
+
+print(fun_fact(5))
