2a25cad484beeafd7ffa0ccf69c8ca25370ffcfe
alokrai0607 <saelalok001@gmail.com>
2024-09-03 11:37:47 +0530

    Sorting

---
+++ list1.py
@@ -0,0 +1,15 @@
+#Accessing Elements
+my_list = [10, 20, 30, 40, 50]
+print(my_list[0])
+
+
+for i in range(0,len(my_list),1):
+    print(my_list[i],end=" ")
+
+
+#DecendingOrder
+my_list = [10, 20, 30, 40, 50]
+print(my_list[-1])
+
+for i in range(len(my_list)-1,-1,-1):
+    print(my_list[i],end=" ")
