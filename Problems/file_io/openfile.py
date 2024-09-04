64b8975d3f732337d259e00386f4b140da4e5725
alokrai0607 <saelalok001@gmail.com>
2024-09-04 14:12:32 +0530

    read,write,append

--- Problems/file_io/openfile.py
+++ Problems/file_io/openfile.py
@@ -5,4 +5,18 @@ f.close()
 
 
 #Way 2
+f = open(r"C:\Users\91904\OneDrive\Desktop\Learning_Python\Problems\file_io\demo.txt", "r")
+line1 =f.readline()
+print(line1)
 
+#append 
+f = open(r"C:\Users\91904\OneDrive\Desktop\Learning_Python\Problems\file_io\demo.txt", "a")
+f.write("Also i want to learn JS")
+print(f)
+f.close()
+
+#Rewrite
+f = open(r"C:\Users\91904\OneDrive\Desktop\Learning_Python\Problems\file_io\demo.txt", "w")
+f.write("I hope all clear")
+print(f)
+f.close()
