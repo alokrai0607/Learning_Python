bbfff86a1a40acae1af34aac5af3fbac378fa747
alokrai0607 <saelalok001@gmail.com>
2024-09-04 10:42:23 +0530

    swapusing3rd

---
+++ Problems/swapusing3rd.py
@@ -0,0 +1,10 @@
+a = int(input("pass here the value of a :  "))
+b = int(input("pass here the value of b :  "))
+temp = ()
+
+temp = a 
+a = b
+b = temp
+
+print("Value of a will be :",a)
+print("VAlue of b will be :",b)
