cf79c371af6e4ac8d1d395ebda8bc295b797c4f1
alokrai0607 <saelalok001@gmail.com>
2024-09-04 11:32:35 +0530

    firstfebo

---
+++ Problems/EvenOrOdd.py
@@ -0,0 +1,8 @@
+# Write a program that checks if a number entered by the user is odd or even
+
+num = int(input("Pass your number here: "))
+
+if num % 2 == 0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
