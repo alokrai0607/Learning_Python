38c03aaa57d0110ef0cc8fdac5444dad32ce4c41
alokrai0607 <saelalok001@gmail.com>
2024-09-04 12:23:36 +0530

    calcavg

---
+++ Problems/calcavg3.py
@@ -0,0 +1,12 @@
+def calcavg():
+    a = int (input("first value : "))
+    b =  int (input("second value : "))
+    c =  int (input("third value : "))
+
+    avg = (a+b+c)/3
+
+    print (avg)
+
+calcavg()
+
+    
