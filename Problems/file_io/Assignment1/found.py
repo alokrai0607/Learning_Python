cd4c9a0e95ec552d443420e56e08d7d08fb89362
alokrai0607 <saelalok001@gmail.com>
2024-09-04 15:28:30 +0530

    Cheackline

--- Problems/file_io/Assignment1/found.py
+++ Problems/file_io/Assignment1/found.py
@@ -5,3 +5,21 @@ with open(r"C:\Users\91904\OneDrive\Desktop\Learning_Python\Problems\file_io\Ass
         print("Found")
     else:
         print("Not Found")
+
+
+
+def check_line():
+    word = "learning"
+    data = True
+    lineNo = 1
+    with open(r"C:\Users\91904\OneDrive\Desktop\Learning_Python\Problems\file_io\Assignment1\newFile.txt","r") as f:
+        while data :
+            data = f.readline()
+            if(word in data):
+                print(f"{word}","is in line no :",lineNo)
+                return
+            lineNo += 1
+
+    return -1
+
+check_line()
