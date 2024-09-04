71aceddb450141fd1936235bf41673a63a6f0e9b
alokrai0607 <saelalok001@gmail.com>
2024-09-04 10:31:56 +0530

    swap without 3rd variable

---
+++ Problems/swap2without3rd.py
@@ -0,0 +1,10 @@
+def swap(a,b):
+    a = int(input ("Please pass the first value Here : "))
+    b = int(input ("Please pass the second value Here : "))
+    a = a + b
+    b = a - b
+    a = a - b
+    print("a will be : ",a)
+    print("b will be : ",b)
+
+#print(swap(4,5))
