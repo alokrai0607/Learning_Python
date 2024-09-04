b6d855e6820e6b267d673f551986c6860ead69cd
alokrai0607 <saelalok001@gmail.com>
2024-09-04 17:43:59 +0530

    const

--- Array/oops/constructor.py
+++ Array/oops/constructor.py
@@ -1,7 +1,9 @@
 class Student :
-    def __init__(self,fullName):
+    def __init__(self,fullName,marks):
         self.name = fullName
+        self.marks = marks
         print("Adding new student in database :  ")
 
-s1 = Student("Alok Kumar Rai")
+s1 = Student("Alok Kumar Rai",96)
 print(s1.name)
+print(s1.marks)
