c2fde9eb2a137d16b4975df3624350955d3261de
alokrai0607 <saelalok001@gmail.com>
2024-09-04 10:49:27 +0530

    Name&Age

---
+++ Problems/nameAge.py
@@ -0,0 +1,8 @@
+#Create a program that takes a user’s name and age as input 
+# and prints a message saying how old they will be in 10 years.
+
+
+name = str(input("Pass name Here : "))
+age = int(input("Pass age Here :  "))
+
+print ({name}," Will be ",{age+10} , "years old after ten year. ")
