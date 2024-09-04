edc8b79217283cd71b1ac4956df694917282ff32
alokrai0607 <saelalok001@gmail.com>
2024-09-04 15:58:42 +0530

    .csv

--- Problems/file_io/Assignment2/main.py
+++ Problems/file_io/Assignment2/main.py
@@ -4,13 +4,13 @@ with open(r"C:\Users\91904\OneDrive\Desktop\Learning_Python\Problems\file_io\Ass
     #print(data)
 
 #1st way
-    # num=""
-    # for i in range(len(data)):
-    #     if(data[i]==","):
-    #         print(num)
-    #         num=""
-    #     else:
-    #         num += data[i]
+    num=""
+    for i in range(len(data)):
+        if(data[i]==","):
+            print(num)
+            num=""
+        else:
+            num += data[i]
     
 #2nd way
 count = 0
