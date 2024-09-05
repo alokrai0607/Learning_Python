64b5c8ff8c1d607bc0372c5b276e1106a4ed7820
alokrai0607 <saelalok001@gmail.com>
2024-09-05 14:29:15 +0530

    superK

---
+++ Problems/super.py
@@ -0,0 +1,21 @@
+class Car :
+    def __init__(self,type):
+        self.type = type
+
+    @staticmethod
+    def start():
+        print("Car Started")
+
+    @staticmethod
+    def stop():
+        print("Car Stopped")
+
+class TyotaCar(Car):
+    def __init__(self,name,type):
+        super().__init__(type)
+        self.name = name
+        super().start()
+
+Car1 = TyotaCar("Fortuner","Electric")
+print(Car1.name)
+print(Car1.start)
