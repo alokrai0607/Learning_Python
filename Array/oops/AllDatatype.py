2d7f75fb4cfddca3843166562cd50cda80599184
alokrai0607 <saelalok001@gmail.com>
2024-09-05 11:18:31 +0530

    all datatype

---
+++ Array/oops/AllDatatype.py
@@ -0,0 +1,52 @@
+#Numerics data type 
+
+#Integer
+num_it = 1000
+print(num_it)
+
+#Float
+num_float = 6.1
+print(num_float)
+
+#Complex
+num_Comp = 1 + 5j
+print(num_Comp)
+
+#Sequence Types :
+
+#String
+my_String = "Hello Dear"
+print (my_String)
+
+#List
+my_List = [1,2,3,4,5]
+print(my_List)
+
+#Tuple
+my_tuple = ("Apple" , "Banana" , "Charry")
+print(my_tuple)
+
+##Mapping Type
+my_dict = {'name': 'Alok Rai', 'age': 28, 'city': 'Varanasi'}
+print(my_dict)
+
+#### Set Types:
+
+#Set
+my_Set = {1,2,3,4,5,6,9,7,8}
+print(my_Set)
+
+# Frozenset
+my_frozenset = frozenset({10, 20, 30 , 60 , 80})
+print(my_frozenset)
+
+
+## Boolean Type:
+
+# Boolean
+is_true = True
+print(is_true)
+
+
+is_false = False
+print(is_false)
