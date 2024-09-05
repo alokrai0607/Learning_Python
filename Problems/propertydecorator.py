d8ad194cced24e8e4ad89ad6ac7d4036f942ff5b
alokrai0607 <saelalok001@gmail.com>
2024-09-05 15:24:39 +0530

    property

--- Problems/propertydecorator.py
+++ Problems/propertydecorator.py
@@ -3,7 +3,11 @@ class Student :
         self.phy = phy
         self.chm = chm
         self.math = math
-        self.percentage = str((self.phy+self.chm+self.math)/3)+"%"
+        
+        @property
+        def percentage(self):
+            return str((self.phy+self.chm+self.math)/3)+"%"
 
 Stu1 = Student(95,99,93)
+Stu1.chm = 100
 print(Stu1.percentage)
